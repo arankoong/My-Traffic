f9fbeea3521300e062c1533d9ce8d65fded27a2a
aran <idin2000@hotmail.com>
2016-07-02 15:00:49 +0700

    Finish as 2 jun 2016

--- app/src/main/java/sku/prakobsant/aran/mytraffic/MainActivity.java
+++ app/src/main/java/sku/prakobsant/aran/mytraffic/MainActivity.java
@@ -1,13 +1,46 @@
 package sku.prakobsant.aran.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMButton = (Button) findViewById(R.id.button);
+
+        //Controller for Button
+        aboutMButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),R.raw.elephant);
+                mediaPlayer.start();
+
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/XxVPPNDlYxQ?list=RDXxVPPNDlYxQ"));
+                startActivity(intent);
+            } //onClick
+        });
+
+    } // Main Method
+} //Main Class นี่คือ คลาสหลัก
+
